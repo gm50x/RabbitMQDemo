e3cd2a06240261dce5a323633a12eb4f9dde278a
gm50x <getuliomagela@outlook.com>
2024-02-17 10:12:50 -0300

    Add project files.

---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new()
+            {
+                Uri = new Uri("amqp://gedai:gedai@localhost:5672"),
+                ClientProvidedName = "RabbitPublisherNET"
+            };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string queueName = "DemoQueue";
+            string routingKey = "demo.rk";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey);
+
+            var limit = 60;
+            for (int i = 0; i < limit; i++)
+            {
+                var message = $"#{i.ToString().PadLeft(limit.ToString().Length, '0')} - Hello YouTube!";
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes(message);
+                var id = Guid.NewGuid();
+                var props = channel.CreateBasicProperties();
+                props.MessageId = id.ToString();
+                channel.BasicPublish(exchangeName, routingKey, props, messageBodyBytes);
+                Console.WriteLine($"Sending \"{message}\" with id {props.MessageId}");
+                Thread.Sleep(1000);
+            }
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ RabbitSubscriber2/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitSubscriber2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new()
+            {
+                Uri = new Uri("amqp://gedai:gedai@localhost:5672"),
+                ClientProvidedName = "RabbitSubscriber2NET"
+            };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string queueName = "DemoQueue";
+            string routingKey = "demo.rk";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey);
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(2.5)).Wait();
+                var body = args.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Quick Message Received: {message} with id {args.BasicProperties.MessageId}");
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+            Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
+
+        }
+    }
+}
